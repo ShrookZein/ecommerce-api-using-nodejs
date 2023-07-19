2d2f920ddb54841148fade89f1518d4f32619d51
ShrookZein <sroukzain7007@gmail.com>
2023-07-19 09:56:41 +0300

    Update ProductService.js

--- services/ProductService.js
+++ services/ProductService.js
@@ -1,73 +1,70 @@
 const slugify = require("slugify");
 const asyncHandler = require("express-async-handler");
 const ApiError = require("../utils/apiError");
-const Category = require("../models/categoryModel");
+const Product = require("../models/ProductModel");
 
-// @desc    Get list of categories
-// @route   GET /api/v1/categories
+// @desc    Get list of products
+// @route   GET /api/v1/products
 // @access  Public
-exports.getCategories = asyncHandler(async (req, res) => {
+exports.getProducts = asyncHandler(async (req, res) => {
   const page = req.query.page * 1 || 1;
   const limit = req.query.limit * 1 || 5;
   const skip = (page - 1) * limit;
 
-  const categories = await Category.find({}).skip(skip).limit(limit);
-  res.status(200).json({ results: categories.length, page, data: categories });
+  const product = await Product.find({}).skip(skip).limit(limit);
+  res.status(200).json({ results: product.length, page, data: product });
 });
 
-// @desc    Get specific category by id
-// @route   GET /api/v1/categories/:id
+// @desc    Get specific Product by id
+// @route   GET /api/v1/products/:id
 // @access  Public
-exports.getCategory = asyncHandler(async (req, res, next) => {
+exports.getProduct = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-  const category = await Category.findById(id);
-  if (!category) {
+  const product = await Product.findById(id);
+  if (!product) {
     // res.status(404).json({ msg: `No category for this id ${id}` });
-    return next(new ApiError(`No category for this id ${id}`, 404));
+    return next(new ApiError(`No Product for this id ${id}`, 404));
   }
-  res.status(200).json({ data: category });
+  res.status(200).json({ data: product });
 });
 
-// @desc    Create category
-// @route   POST  /api/v1/categories
+// @desc    Create Product
+// @route   POST  /api/v1/products
 // @access  Private
-exports.createCategory = asyncHandler(async (req, res) => {
-  //const name = req.body.name;
-  const { name } = req.body;
-  const category = await Category.create({ name, slug: slugify(name) });
-  res.status(201).json({ data: category });
+exports.createProduct = asyncHandler(async (req, res) => {
+  req.body.slug = slugify(req.body.title);
+  const product = await Product.create(req.body);
+  res.status(201).json({ data: product });
 });
 
-// @desc    Update specific category
-// @route   PUT /api/v1/categories/:id
+// @desc    Update specific Product
+// @route   PUT /api/v1/products/:id
 // @access  Private
-exports.updateCategory = asyncHandler(async (req, res, next) => {
+exports.updateProduct = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-  const { name } = req.body;
+  req.body.slug = slugify(req.body.title);
 
-  const category = await Category.findOneAndUpdate(
-    { _id: id },
-    { name, slug: slugify(name) },
-    { new: true }
-  );
+  const product = await Product.findOneAndUpdate({ _id: id }, req.body, {
+    new: true,
+  });
 
-  if (!category) {
-    // res.status(404).json({ msg: `No category for this id ${id}` });
-    return next(new ApiError(`No category for this id ${id}`, 404));
+  if (!product) {
+    // res.status(404).json({ msg: `No product for this id ${id}` });
+    return next(new ApiError(`No product for this id ${id}`, 404));
   }
-  res.status(200).json({ data: category });
+  res.status(200).json({ data: product });
 });
 
-// @desc    Delete specific category
-// @route   DELETE /api/v1/categories/:id
+// @desc    Delete specific Product
+// @route   DELETE /api/v1/products/:id
 // @access  Private
-exports.deleteCategory = asyncHandler(async (req, res, next) => {
+exports.deleteProduct = asyncHandler(async (req, res, next) => {
   const { id } = req.params;
-  const category = await Category.findByIdAndDelete(id);
+  const product = await Product.findByIdAndDelete(id);
 
-  if (!category) {
-    // res.status(404).json({ msg: `No category for this id ${id}` });
-    return next(new ApiError(`No category for this id ${id}`, 404));
+  if (!product) {
+    // res.status(404).json({ msg: `No Product for this id ${id}` });
+    return next(new ApiError(`No product for this id ${id}`, 404));
   }
   res.status(204).send();
 });
